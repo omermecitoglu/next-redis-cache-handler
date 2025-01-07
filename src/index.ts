fe864458b3350100c46d79c69d5a90ec080fb757
Omer Mecitoglu <omer.mecitoglu@gmail.com>
2025-01-07 12:12:22 +0700

    feat: cache handler

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,59 @@
-export function exampleFunction() {
-  console.log(" This is an example function ");
+import Redis from "ioredis";
+
+type CacheStructure = {
+  value: unknown,
+  lastModified: number,
+  tags: string[],
+};
+
+type CacheContext = {
+  fetchCache: boolean,
+  revalidate: boolean,
+  fetchUrl: string,
+  fetchIdx: number,
+  tags: string[],
+};
+
+export default class CacheHandler {
+  options: unknown;
+  redis: Redis;
+
+  constructor(options: unknown) {
+    this.options = options;
+    this.redis = new Redis({
+      host: process.env.REDIS_HOST,
+      port: parseInt(process.env.REDIS_PORT || "6379"),
+    });
+  }
+
+  async get(key: string) {
+    const cache = await this.redis.get(`key:${key}`);
+    return cache && JSON.parse(cache);
+  }
+
+  async set(key: string, cache: unknown, ctx: CacheContext) {
+    await this.redis.set(`key:${key}`, JSON.stringify({
+      value: cache,
+      lastModified: Date.now(),
+      tags: ctx.tags,
+    } as CacheStructure));
+    for (const tag of ctx.tags) {
+      await this.redis.sadd(`tag:${tag}`, key);
+    }
+  }
+
+  async revalidateTag(tags: string[] | string) {
+    tags = [tags].flat();
+    for (const tag of tags) {
+      const keys = await this.redis.smembers(`tag:${tag}`);
+      for (const key of keys) {
+        await this.redis.del(`key:${key}`);
+      }
+      await this.redis.del(`tag:${tag}`);
+    }
+  }
+
+  // If you want to have temporary in memory cache for a single request that is reset
+  // before the next request you can leverage this method
+  // public resetRequestCache() {}
 }
