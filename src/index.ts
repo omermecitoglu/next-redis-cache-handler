95e99106da358da08968514437d1b30c76f2f2da
Omer Mecitoglu <omer.mecitoglu@gmail.com>
2026-04-10 17:47:02 +0700

    feat: username and password

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,8 @@ const cacheMap = new Map<string, CacheStructure>();
 const redis = new Redis({
   host: process.env.REDIS_HOST,
   port: parseInt(process.env.REDIS_PORT || "6379"),
+  username: process.env.REDIS_USERNAME,
+  password: process.env.REDIS_PASSWORD,
   connectionName: process.title,
 });
 redis.on("connect", () => {
